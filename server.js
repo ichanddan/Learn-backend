602dd5714dd50ed48d30722b2c5b4d940156fdc4
ichanddan <code.us.cm@gmail.com>
2024-05-06 00:04:58 +0530

    Data send and find in database and body-parser

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './DB/index.js';
+import { User } from './Models/user.models.js';
+import bodyParser from 'body-parser';
+
 dotenv.config()
 
 const app = express()
 const Port = process.env.PORT || 8000;
 connectDB()
+app.use(bodyParser.json())
 
-app.get('/home', (req , res)=>{
-    res.send("Hello Dosto")
+app.post('/user', async(req , res)=>{
+    try {
+        const data = req.body
+        const newda = new User(data)
+        const finnal = await newda.save()
+        res.status(200).json(finnal)
+        console.log("Data send succesfully", finnal)
+    } catch (error) {
+        console.log("Data not send", error)
+    }
+})
+
+app.get('/user', async (req , res)=>{
+    try {
+        const data = await User.find()
+        res.status(200).json(data)
+        console.log("Data Fatched")
+    } catch (error) {
+        console.log("Data not Fatched", error)
+    }
 })
 
 app.listen(Port, ()=>{
