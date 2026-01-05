1c1c813bf7da302015c9542ccc54349c49825ac7
ichanddan <code.us.cm@gmail.com>
2026-01-05 21:19:23 +0530

    learn lenth funcation

--- python/learning-basics/main.py
+++ python/learning-basics/main.py
@@ -1,9 +1,12 @@
 string = "this is string"
-number=1
+number = 1
 boo = True
 print(string, number, boo)
 
 # escape sequence character
 # \n for new line and \t for space
-st = 'This is a string \n new project staring and more information \t xman information '
-print(st)
+st = "This is a string \n new project staring and more information \t xman information "
+print(st + string)
+
+# len funcation for get lenght of string
+print(len(st))
