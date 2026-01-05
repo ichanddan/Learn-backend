c5bf10a674aabeb45cff65dc64d6ee830c0d0d95
ichanddan <code.us.cm@gmail.com>
2026-01-05 21:17:37 +0530

    learn brek and space

--- python/learning-basics/main.py
+++ python/learning-basics/main.py
@@ -2,3 +2,8 @@ string = "this is string"
 number=1
 boo = True
 print(string, number, boo)
+
+# escape sequence character
+# \n for new line and \t for space
+st = 'This is a string \n new project staring and more information \t xman information '
+print(st)
